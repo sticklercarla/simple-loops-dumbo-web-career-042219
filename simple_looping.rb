9df5036fe4e5e6ff4334ee451fb77a07fa13c340
Carla Stickler <carlastickler@gmail.com>
2019-04-05 22:46:13 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -12,7 +12,7 @@ def loop_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   loop do 
     counter = phrase += 1 
-    if counter <=4
+    if counter <= 7
       break
     end
   end

7ab356b2ac3db40e39af1bb4d8bee4daf0c2cecf
Carla Stickler <carlastickler@gmail.com>
2019-04-05 23:15:49 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -34,7 +34,7 @@ def while_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   counter = 0
   while counter == number_of_times
-    puts phrase
+    puts counter
     counter += 1
   end
 end
